2d7c199f1930c20c8e7253e08d6e721ec1b2a245
andres-thefirst <darkshadowskull@gmail.com>
2019-03-10 22:50:24 -0600

    Firebase auth with expo

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
+import SignUpForm from './components/SignUpForm';
+import SignInForm from './components/SignInForm';
+
+export default class App extends React.Component {
+  componentDidMount() {
+    const config = {
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: ""
+    };
+    firebase.initializeApp(config);
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <SignUpForm />
+        <SignInForm />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
